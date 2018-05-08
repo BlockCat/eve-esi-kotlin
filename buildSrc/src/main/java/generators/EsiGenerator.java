4605421aa7bc8cd3a72807060ca047d3f2997424
BlockCat <zinoonomiwo@gmail.com>
2018-05-08 19:07:24 +0200

    Add templates

--- buildSrc/src/main/java/generators/EsiGenerator.java
+++ buildSrc/src/main/java/generators/EsiGenerator.java
@@ -1,7 +1,10 @@
 package generators;
 
 
+import io.swagger.codegen.SupportingFile;
 import io.swagger.codegen.languages.KotlinClientCodegen;
+
+import java.io.File;
 
 public class EsiGenerator extends KotlinClientCodegen {
 
@@ -9,6 +12,11 @@ public class EsiGenerator extends KotlinClientCodegen {
     public void processOpts() {
         super.processOpts();
 
+        final String infrastructureFolder = (sourceFolder + File.separator + packageName + File.separator + "infrastructure").replace(".", "/");
+
+        supportingFiles.add(new SupportingFile("infrastructure/ApiClient.kt.mustache", infrastructureFolder, "ApiClient.kt"));
+        supportingFiles.add(new SupportingFile("infrastructure/ServiceBuilder.kt.mustache", infrastructureFolder, "ServiceBuilder.kt"));
+        supportingFiles.add(new SupportingFile("infrastructure/AccessToken.kt.mustache", infrastructureFolder, "AccessToken.kt"));
 
     }
 }
