b5f19899967adb49eec669218578789d872079a9
BlockCat <zinoonomiwo@gmail.com>
2018-05-08 19:01:42 +0200

    add generator for infrastructure

---
+++ buildSrc/src/main/java/generators/EsiGenerator.java
@@ -0,0 +1,14 @@
+package generators;
+
+
+import io.swagger.codegen.languages.KotlinClientCodegen;
+
+public class EsiGenerator extends KotlinClientCodegen {
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+
+
+    }
+}
